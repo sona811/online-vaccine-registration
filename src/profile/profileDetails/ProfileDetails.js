6e4ef2e915817ba1e591a9bee73f4149fc53a490
sona811 <sonali.ubale811@gmail.com>
2021-08-18 01:20:14 +0530

    added functional component

--- src/profile/profileDetails/ProfileDetails.js
+++ src/profile/profileDetails/ProfileDetails.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './ProfileDetails.css';
 
-export class ProfileDetails extends React.Component {
-
-    render() {
-        return (
-            <>
+export const ProfileDetails = () => {
+    return(
+        <>
                 <img src='' alt='' height='100px' width='100px' />
                 <h3 > Sonali Ubale </h3>
                 <label > Introvert </label>
             </>
-        )
-    }
+    )
 }
